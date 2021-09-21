021f79b5f134b7b0cf9b8b99f2ba2676122a7128
Nick Johnson <arachnid@notdot.net>
2021-09-21 14:27:38 +1200

    Add other fields to test airdrop

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,16 @@ task("maketree", "Generates a merkle airdrop tree").setAction(async () => {
   let shardNybbles = 1;
   if(hre.network.tags.test) {
     const signers = await ethers.getSigners();
-    airdrops = signers.map((signer) => [signer.address, {balance: '1000000000000000000000000'}]);
+    airdrops = signers.map((signer) => [
+      signer.address,
+      {
+        past_tokens: '500000000000000000000000',
+        future_tokens: '500000000000000000000000',
+        longest_owned_name: '0x04f740db81dc36c853ab4205bddd785f46e79ccedca351fc6dfcbd8cc9a33dd6', // keccak256('test')
+        last_expiring_name: '0x04f740db81dc36c853ab4205bddd785f46e79ccedca351fc6dfcbd8cc9a33dd6',
+        balance: '1000000000000000000000000'
+      }
+    ]);
   } else {
     const fs = require('fs');
     airdrops = fs.readFileSync('airdrop.json', {encoding: 'utf-8'}).split('\n').filter((x) => x.length > 0).map((line) => {
