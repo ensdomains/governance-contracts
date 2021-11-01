4b6a2bbbfc057ee5504757036ea84a7bbf345cdd
Nick Johnson <arachnid@notdot.net>
2021-11-02 09:40:20 +1300

    Tweaks to distribute script, Ropsten deployment

--- deploy/20_distribute_tokens.js
+++ deploy/20_distribute_tokens.js
@@ -9,23 +9,24 @@ module.exports = async ({getNamedAccounts, deployments}) => {
   const tokenLock = await ethers.getContract('TokenLock');
 
   // Transfer locked tokens to the tokenlock
-  const lockedDAOTokens = oneToken.mul(config.LOCKED_DAO_TOKENS);
-  await ensToken.approve(tokenLock.address, lockedDAOTokens);
-  await tokenLock.lock(timelockController.address, lockedDAOTokens);
-
-  // Transfer contributor tokens to the contributor address
-  const totalContributorTokens = oneToken.mul(config.TOTAL_CONTRIBUTOR_TOKENS);
-  await ensToken.transfer(config.CONTRIBUTOR_ADDRESS, totalContributorTokens);
+  if((await tokenLock.lockedAmounts(timelockController.address)).eq(0)) {
+    const lockedDAOTokens = oneToken.mul(config.LOCKED_DAO_TOKENS);
+    await (await ensToken.approve(tokenLock.address, lockedDAOTokens)).wait();
+    await (await tokenLock.lock(timelockController.address, lockedDAOTokens)).wait();
+  }
 
   // Transfer free tokens to the timelock controller
+  const totalContributorTokens = oneToken.mul(config.TOTAL_CONTRIBUTOR_TOKENS);
   const balance = await ensToken.balanceOf(deployer);
-  await ensToken.transfer(timelockController.address, balance);
+  if(balance.gt(totalContributorTokens)) {
+    await (await ensToken.transfer(timelockController.address, balance.sub(totalContributorTokens))).wait();
+  }
 
   // Print out balances
   const daoBalance = await ensToken.balanceOf(timelockController.address);
   console.log(`Token balances:`);
   console.log(`  DAO: ${daoBalance.div(oneToken).toString()}`);
-  const contributorBalance = await ensToken.balanceOf(config.CONTRIBUTOR_ADDRESS);
+  const contributorBalance = await ensToken.balanceOf(deployer);
   console.log(`  Contributors: ${contributorBalance.div(oneToken).toString()}`);
   const airdropBalance = await ensToken.balanceOf(ensToken.address);
   console.log(`  Airdrop: ${airdropBalance.div(oneToken).toString()}`);
@@ -36,6 +37,9 @@ module.exports = async ({getNamedAccounts, deployments}) => {
   console.log(`    TOTAL: ${lockedDaoBalance.div(oneToken).toString()}`);
   const total = daoBalance.add(contributorBalance).add(airdropBalance).add(tokenlockBalance);
   console.log(`  TOTAL: ${total.div(oneToken).toString()}`);
+
+  return true;
 };
 module.exports.tags = ['distribute'];
 module.exports.dependencies = ['ENSToken', 'TimelockController', 'TokenLock'];
+module.exports.id = 'distribute';
