fcc707a43b600ce3f9092d084666f00c9be486bf
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2023-07-21 20:57:51 +0200

    add coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,8 @@ const config = require('./config');
 require('hardhat-deploy');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
 
 // Load environment variables from .env file. Suppress warnings using silent
 // if this file is missing. dotenv will never modify any environment variables
@@ -25,15 +27,28 @@ module.exports = {
   solidity: '0.8.7',
   settings: {
     optimizer: {
-      enabled: true // runs: 200 by default
-    }
+      enabled: true, // runs: 200 by default
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    coinmarketcap: '08cd3ec6-0b9b-4b4b-9906-f215c98f5896',
   },
   namedAccounts: {
     deployer: {
       default: 0,
     },
-    delegatee: {
+    alice: {
       default: 1,
+    },
+    bob: {
+      default: 2,
+    },
+    charlie: {
+      default: 3,
+    },
+    dave: {
+      default: 4,
     },
   },
   networks: {
@@ -43,13 +58,13 @@ module.exports = {
       allowUnlimitedContractSize: true,
     },
     mainnet: {
-      url: "http://localhost:8545/",
+      url: 'http://localhost:8545/',
       chainId: 1,
       accounts: real_accounts,
-      maxPriorityFeePerGas: 1000000000
+      maxPriorityFeePerGas: 1000000000,
     },
     tenderly: {
-      url: "https://rpc.tenderly.co/fork/bd704e15-7f2c-4f12-8c1a-9bedf536c336"
-    }
+      url: 'https://rpc.tenderly.co/fork/bd704e15-7f2c-4f12-8c1a-9bedf536c336',
+    },
   },
 };
