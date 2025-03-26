d0a4bbec901d4642cd8e0aba154e30346982b669
Nick Johnson <arachnid@notdot.net>
2025-03-26 14:48:57 +0000

    Fix mock UniversalResolver return type

--- contracts/test/MockUniversalResolver.sol
+++ contracts/test/MockUniversalResolver.sol
@@ -33,15 +33,14 @@ contract MockUniversalResolver is IExtendedResolver, ERC165 {
     function reverse(bytes calldata, uint256 coinType) external view returns (
         string memory resolvedName,
         address resolvedAddress,
-        address reverseResolver,
-        address contractAddress
+        address reverseResolver
     ) {
         if (shouldReturnEmptyName) {
             // Return empty name for the "should handle unresolved names" test
-            return ("", address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0), address(0));
+            return ("", address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0));
         } else {
             // Return "test.eth" for the "should return the correct token URI" test
-            return ("test.eth", address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0), address(0));
+            return ("test.eth", address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0));
         }
     }
     
